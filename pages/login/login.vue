3337727602c463007fc1872eef13fab3ce786502
sxy8077 <70307543+sxy8077@users.noreply.github.com>
2020-11-14 09:41:17 +0800

    Update login.vue

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,33 +1,38 @@
-<style lang="scss" scoped>
-</style>
+
 <template>
-    <view>
+	<form class='loginView' @submit="bindLogin">
         <view class="input-box">
             <view class="input-item">
                 <view class="input-label">账号</view>
 				<view class="input-body">
-					<input v-model="userName"  type="text" placeholder="请输入账号admin" class="input" >
+					<input   type="text" placeholder="请输入账号admin" class="input" name="nameValue" >
 				</view>
             </view>
             <view class="input-item">
 				<view class="input-label">密码</view>
 				<view class="input-body">
-					<input v-model="password" type="text" :password="isHidePassword?true:false" style="margin-right: 50upx;" placeholder="请输入密码1234567" maxlength="20"  class="input" />
+					<input type="text" :password="isHidePassword?true:false" style="margin-right: 50upx;" placeholder="请输入密码1234567" maxlength="20"  class="input" name="passwordValue"  />
 					<image @click="isHidePasswordClick" class="eye" :src="isHidePassword?'/static/img/attention.png':'/static/img/attention_forbid.png'"></image>
 				</view>
             </view>
         </view>
-        <button class="button" @click="submit">登录</button>
-    </view>
+        <button :loading="loading" class="button" formType="submit">登录</button>
+	</form>
 </template>
+
 <script>
-	import {checkPwd} from "@/common/common.js"
+	// import {checkPwd} from "@/common/common.js"
+	import {
+	    mapMutations
+	} from 'vuex';
+	
 	export default {
 		data() {
 			return {
 				isHidePassword:true,
-				userName:"",
-				password:""
+				loading: false
+				// userName:"",
+				// password:""
 			};
 		},
 		onLoad() {
@@ -36,29 +41,101 @@
 			isHidePasswordClick(){
 				this.isHidePassword=!this.isHidePassword
 			},
-			submit() {
-				// if(!checkuserName(this.userName)){
-				// 	return;
-				// };
-				if(!checkPwd(this.password)){
-					return;
-				};
-				/*
-				* 登录逻辑
-				*/
-				this.$store.commit('update',['isLogin',true]);
-				// uni.reLaunch({
-				// 	url:'/pages/index/index'
-				// })
-					if(this.userName === "admin" && this.password === "1234567"){
-						 // 验证成功跳转目标页面 
-						 uni.reLaunch({
-						 	url:'/pages/index/index'
-						 })
-					}
-				
+			
+			bindLogin(e) {
+					console.log(e);
+			        this.loading = true;
+			        let name = e.detail.value.nameValue,
+			            password = e.detail.value.passwordValue;
+			
+			        // if (!/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/.test(name)) {
+			        //     this.loading = false;
+			        //     uni.showModal({
+			        //         content: "请输入正确账号",
+			        //         showCancel: false
+			        //     })
+			        //     return;
+			        // }
+			
+			        if (password.length < 6) {
+			            this.loading = false;
+			            uni.showModal({
+			                content: "密码大于5位",
+			                showCancel: false
+			            })
+			            return;
+			        }
+			
+			        uni.request({
+			            url: `${this.$serverUrl}/login.php`,
+			            header: {
+			                "Content-Type": "application/x-www-form-urlencoded"
+			            },
+			            data: {
+			                "username": name,
+			                "password": password
+			            },
+			            method: "POST",
+			            success: (e) => {
+							console.log(66,e)
+			                if (e.statusCode !== 200) {
+			                    uni.showModal({
+			                        content: "用户名密码错误！",
+			                        showCancel: false
+			                    });
+			                    return;
+			                }
+			                if (e.data.code === 0) {
+			                    this.login(e.data);
+								// uni.redirectTo({
+								// 	url:'../index/index'
+								// })
+								uni.reLaunch({
+									url:'../index/index'
+								})
+			                } else {
+			                    uni.showModal({
+			                        content: e.data.message,
+			                        showCancel: false
+			                    })
+			                }
+			            },
+			            fail: (e) => {
+			                uni.showModal({
+			                    content: "请求失败，请重试！",
+			                    showCancel: false
+			                })
+			            },
+			            complete: () => {
+			                this.loading = false;
+			            }
+			        })
+			    },
+			    ...mapMutations(['login'])
 			}
-		}
+			
+			// submit() {
+			// 	// if(!checkuserName(this.userName)){
+			// 	// 	return;
+			// 	// };
+			// 	if(!checkPwd(this.password)){
+			// 		return;
+			// 	};
+			// 	/*
+			// 	* 登录逻辑
+			// 	*/
+			// 	this.$store.commit('update',['isLogin',true]);
+			// 	// uni.reLaunch({
+			// 	// 	url:'/pages/index/index'
+			// 	// })
+			// 		if(this.userName === "admin" && this.password === "1234567"){
+			// 			 // 验证成功跳转目标页面 
+			// 			 uni.reLaunch({
+			// 			 	url:'/pages/index/index'
+			// 			 })
+			// 	}	
+			// }
+		
 	}	
 </script>
 
