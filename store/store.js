1086e49beb9d4222906979b6a7b9aa98f2504c52
sxy8077 <70307543+sxy8077@users.noreply.github.com>
2020-11-14 09:42:49 +0800

    Update store.js

--- store/store.js
+++ store/store.js
@@ -5,12 +5,46 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
 	state: {
-		isLogin :uni.getStorageSync('isLogin')?true:false,
+		isLogin :false,
+		// isLogin :uni.getStorageSync('isLogin')?true:false,
+		uerInfo: {}
+		// hasLogin: false
 	},
 	mutations: {
-		update(state,[key,value]){
-			state[key]=value;
+		// update(state,[key,value]){
+		// 	state[key]=value;
+		// },
+		login(state, provider) {
+			state.isLogin = true
+			// state.hasLogin = true
+			state.uerInfo.token = provider.token
+			state.uerInfo.userName = provider.user_name
+			uni.setStorage({
+				key: 'uerInfo',
+				data: provider
+			})
+			// uni.redirectTo({
+			// 	url:'/pages/index/index.vue'
+			// })
 		},
+		logout(state) {
+			state.isLogin = false
+			// state.hasLogin = false
+			state.uerInfo = {}
+			uni.removeStorage({
+				key: 'uerInfo'
+			});
+			// uni.reLaunch({
+			// 	url:'/pages/login/login.vue'
+			// })
+		},
+		// isLogin(state){
+		// 	if(!state.isLogin){
+		// 		uni.redirectTo({
+		// 			url:'../pages/index/index.vue'
+		// 		})
+		// 	}
+		// }
 	}
 })
 
