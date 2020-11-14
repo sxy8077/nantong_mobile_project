2fffeae857d7dc89bc097a282e076694e6ec6394
sxy8077 <70307543+sxy8077@users.noreply.github.com>
2020-11-14 09:44:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ Vue.prototype.$store=store;
 Vue.config.productionTip = false
 
 App.mpType = 'app'
+Vue.prototype.$serverUrl = 'https://www.dcloud.io/demo'
 
 const app = new Vue({
+	store,
     ...App
 })
 app.$mount()
