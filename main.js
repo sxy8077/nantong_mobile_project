ac9179b45d8513d419cc90e1799ea067f5e96a85
sxy8077 <807713550@qq.com>
2020-11-15 14:09:05 +0800

    sxy

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import store from '@/store/store'
+import { myRequest } from 'util/api.js'
 
 Vue.prototype.$store=store;
 Vue.config.productionTip = false
+Vue.prototype.$myRequest = myRequest
 
 App.mpType = 'app'
 Vue.prototype.$serverUrl = 'https://www.dcloud.io/demo'
