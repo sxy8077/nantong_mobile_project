d235963f1fc5ab531137fc55e8c5ac094de670b8
He Haitao <510540769@qq.com>
2020-11-25 10:28:53 +0800

    haitao

--- components/choose.vue
+++ components/choose.vue
@@ -1,26 +1,27 @@
 <template>
 	<view class="chooseList">
+		<view class="title">{{title}}</view>
 		<view class="chooseItem" @click="navigator(index)"  v-for="(item, index) in chooseItem" :key="index">
 			<view class="left">
 				<!-- #ifdef H5 -->
-				<image :src= "item.H5imgUrl" class="iconRight"></image>
+				<image :src= "item.H5imgUrl" class="iconLeft"></image>
 				<!-- #endif -->
 				<!-- #ifdef MP-WEIXIN -->
-				<image :src="item.WEIimgUrl" class="iconRight"></image>
+				<image :src="item.WEIimgUrl" class="iconLeft"></image>
 				<!-- #endif -->
 				<!-- #ifdef APP-PLUS -->
-				<image :src="item.H5imgUrl" class="iconRight"></image>
+				<image :src="item.H5imgUrl" class="iconLeft"></image>
 				<!-- #endif -->
-				<text style="color: #fff">{{item.name}}</text>
+				<text class="name">{{item.name}}</text>
 			</view>
-			<image src="../static/icon/arrow.png" class="iconLeft"></image>
+			<image src="../static/icon/arrow.png" class="iconRight"></image>
 		</view>
 	</view>
 </template>
 
 <script>
 	export default{
-		props: ['chooseItem'],
+		props: ['chooseItem', 'title'],
 		methods: {
 			navigator(index) {
 				this.$emit("navigatorTo", index);
@@ -31,24 +32,47 @@
 	
 <style lang="scss">
 	.chooseList{
+		background: $background-color;
 		.chooseItem{
+			width: 550rpx;
+			height: 100rpx;
+			margin: 0 auto;
+			margin-bottom: 80rpx;
+			border-radius: 20rpx;
 			padding: 20rpx;
-			background: $background-color;
+			background: #fff;
 			font-size: 50rpx;
-			margin-top: 10px;
 			display: flex;
 			justify-content: space-between;
-			.iconRight{
-				width: 80rpx;
-				height: 80rpx;
+			box-shadow: 7rpx 7rpx 15rpx  #3b466c;
+			.iconLeft{
+				width: 60rpx;
+				height: 60rpx;
 				margin-right: 25rpx;
+				margin-left: 20rpx;
 				vertical-align: middle;    
 			}
-			.iconLeft{
-				width: 80rpx;
-				height: 80rpx;
-				vertical-align: middle;  
+			.iconRight{
+				width: 40rpx;
+				height: 40rpx;
+				vertical-align: middle;
+				margin-top: 30rpx;
 			}
+			.left
+			{
+				.name{
+					color: #3b466c;
+					line-height: 95rpx;
+				}
+			}
+		}
+		.title{
+			height: 140px;
+			text-align: center;
+			line-height: 140px;
+			font-size: 30px;
+			letter-spacing: 10px;
+			color: #fff;
 		}
 	}
 </style>
---
+++ pages/basicInfo/engineInfo/booleen-single-table.vue
@@ -0,0 +1,173 @@
+<template>
+	<view class="wrap">
+		<view class="h1">简单table</view>
+		<view class="div-table">
+			<view class="tr">
+				<view class="td" style="width: 200rpx; t">111</view>
+			</view>
+			<view class="tr">
+				<view class="td">1</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss">
+.h1 {
+	text-align: left;
+	font-size: 30upx;
+	font-weight: bold;
+	padding: 10upx 0;
+}
+.wrap {
+	margin: 20upx;
+}
+$div-table-border-color: #666;
+$div-table-border-width: 1upx;
+
+.div-table {
+	display: table;
+	width: 100%;
+	height: 100%;
+	border: $div-table-border-width solid $div-table-border-color;
+	box-sizing: border-box;
+	table-layout: fixed;
+	position: relative;
+	.celspan {
+		display: table;
+		width: 100%;
+		height: 100%;
+		.td {
+			display: table-cell;
+			border: none !important;
+		}
+		.td + .td {
+			border-left: $div-table-border-width solid $div-table-border-color !important;
+		}
+	}
+	.rowspan {
+		display: table;
+		width: 100%;
+		height: 100%;
+		.tr {
+			display: table-row;
+			border: none !important;
+		}
+	}
+
+	&.fixed-thead {
+		.tbody,
+		.thead,
+		.tr,
+		.th,
+		.td {
+			display: block;
+		}
+		.tr,
+		.th {
+			&:after {
+				content: '';
+				display: block;
+				visibility: hidden;
+				clear: both;
+				height: 0;
+			}
+		}
+		.td {
+			float: left;
+			width: 33.33%;
+		}
+		.tbody {
+			height: 400upx;
+			overflow-y: auto;
+			overflow-x: hidden;
+		}
+	}
+
+	.tr,
+	.th {
+		display: table-row;
+		& + .tr,
+		& + .th {
+			.td,
+			.th {
+				border-top: $div-table-border-width solid $div-table-border-color;
+				word-break: break-word;
+			}
+		}
+	}
+	.rowspan {
+		.tr,
+		.th {
+			display: table-row;
+			.td,
+			.th {
+				border: none;
+			}
+			& + .tr,
+			& + .th {
+				.td,
+				.th {
+					border-top: $div-table-border-width solid $div-table-border-color;
+				}
+			}
+		}
+	}
+	.td {
+		display: table-cell;
+		vertical-align: middle;
+		text-align: center;
+		box-sizing: border-box;
+		padding: 10upx;
+		&.noPadding {
+			padding: 0;
+		}
+		& + .td {
+			border-left: $div-table-border-width solid $div-table-border-color;
+		}
+	}
+	.th .td {
+		font-weight: bold;
+	}
+	.tbody {
+		display: table-row-group;
+	}
+	.thead {
+		display: table-header-group;
+		.tr,
+		.th {
+			.td,
+			.th {
+				border-bottom: $div-table-border-width solid $div-table-border-color;
+			}
+		}
+	}
+	.tfoot {
+		display: table-footer-group;
+		.tr,
+		.th {
+			.td,
+			.th {
+				border-top: $div-table-border-width solid $div-table-border-color;
+			}
+		}
+	}
+	.colgroup {
+		display: table-column-group;
+	}
+	.col {
+		display: table-column;
+	}
+	.caption {
+		display: table-caption;
+	}
+}
+</style>
